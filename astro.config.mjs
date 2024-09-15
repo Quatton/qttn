eac147945246f4669518489e8e7e54690b451d06
Quatton <atomicatives@gmail.com>
2024-09-15 21:22:47 +0900

    stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,16 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
+	integrations: [
+		tailwind({
+			applyBaseStyles: false,
+		}),
+	],
+
+	output: "static",
+	adapter: cloudflare(),
 });
