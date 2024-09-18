d755e54e7e2aef111578687316b4d64bc7fb8ab5
Quatton <atomicatives@gmail.com>
2024-09-19 03:52:03 +0900

    refresh

--- src/actions/const.ts
+++ src/actions/const.ts
@@ -1,9 +1,12 @@
 import { keys } from "@/lib/const/rules";
-import { defineAction } from "astro:actions";
+import { ActionError, defineAction } from "astro:actions";
 import { z } from "astro/zod";
 import { randomUUID } from "node:crypto";
 import { db } from "@/db/drizzle";
-import { sql } from "drizzle-orm";
+import { asc, eq, inArray, sql } from "drizzle-orm";
+import { Words, type Word } from "@/db/schema";
+import { site } from "@/config/site";
+import type { Definition } from "@/lib/const/dictionary";
 
 export const game = {
   new: defineAction({
@@ -15,22 +18,89 @@ export const game = {
     },
   }),
   words: defineAction({
-    input: z
-      .object({
-        max: z.number().int().positive().default(10),
-      })
-      .or(z.void()),
-    handler: async (inp, ctx) => {
-      const input = {
-        max: 10,
-      };
-      return await db.query.Words.findMany({
-        limit: input.max,
-        orderBy: (fields, { asc }) => [
-          asc(sql`RANDOM()`),
-          asc(fields.sampled_count),
-        ],
+    input: z.object({
+      gameId: z.string().uuid(),
+      max: z.number().int().positive().default(10),
+    }),
+    handler: async (input, ctx) => {
+      const words = await db
+        .select()
+        .from(Words)
+        .limit(input.max)
+        .orderBy(asc(sql`RANDOM()`), asc(Words.sampled_count));
+
+      await db
+        .update(Words)
+        .set({
+          sampled_count: sql`${Words.sampled_count} + 1`,
+          rejected_rate: sql`${Words.rejected_count} / ${Words.sampled_count}`,
+        })
+        .where(
+          inArray(
+            Words.id,
+            words.map((word) => word.id),
+          ),
+        );
+
+      ctx.cookies.set(`const:${input.gameId}`, JSON.stringify(words), {
+        expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),
+        path: "/",
+        domain: `.${ctx.url.hostname}`,
+        secure: import.meta.env.PROD,
       });
+
+      return words;
+    },
+  }),
+  swapOut: defineAction({
+    accept: "form",
+    input: z.object({
+      gameId: z.string().uuid(),
+      wordId: z.number().int(),
+    }),
+    handler: async (input, ctx) => {
+      const words = ctx.cookies.get(`const:${input.gameId}`)?.json() as Word[];
+
+      if (!words) {
+        throw new ActionError({
+          code: "NOT_FOUND",
+          message: "Game not found",
+        });
+      }
+
+      const word = words.find((w) => w.id === input.wordId);
+
+      if (!word) {
+        throw new ActionError({
+          code: "NOT_FOUND",
+          message: "Word not found",
+        });
+      }
+
+      await db
+        .update(Words)
+        .set({
+          rejected_count: sql`${Words.rejected_count} + 1`,
+          rejected_rate: sql`CASE WHEN ${Words.sampled_count} = 0 THEN 0 ELSE (${Words.rejected_count} + 1) / ${Words.sampled_count} END`,
+        })
+        .where(eq(Words.id, word.id));
+
+      const [newWord] = await db
+        .select()
+        .from(Words)
+        .orderBy(asc(sql`RANDOM()`))
+        .limit(1);
+
+      const newWords = words.map((w) => (w.id === input.wordId ? newWord : w));
+
+      ctx.cookies.set(`const:${input.gameId}`, JSON.stringify(newWords), {
+        expires: new Date(Date.now() + 1000 * 60 * 60 * 24),
+        path: "/",
+        domain: `.${ctx.url.hostname}`,
+        secure: import.meta.env.PROD,
+      });
+
+      return newWords;
     },
   }),
 };
--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -1,10 +1,11 @@
 import { createClient } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
 import * as schema from "./schema";
+import { DefaultLogger } from "drizzle-orm";
 
 const libsql = createClient({
   url: import.meta.env.TURSO_DATABASE_URL,
   authToken: import.meta.env.TURSO_AUTH_TOKEN,
 });
 
-export const db = drizzle(libsql, { schema });
+export const db = drizzle(libsql, { schema, logger: new DefaultLogger() });
