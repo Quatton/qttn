6b791b910ece91651ca9a4cda8e619f2800ec0a0
Quatton <atomicatives@gmail.com>
2024-09-21 13:00:20 +0900

    let's try just next

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,5 @@
 import type { MiddlewareHandler } from "astro";
 import { site } from "./config/site";
-import { rewrite } from "@vercel/edge";
 
 const subdomains = ["const"];
 const ignorePattern = /\/api|\/[^/]+\.[^/]+|\/_actions/;
@@ -8,7 +7,7 @@ const ignorePattern = /\/api|\/[^/]+\.[^/]+|\/_actions/;
 export const onRequest: MiddlewareHandler = (context, next) => {
   // if the last part of the path is file name like, [name].[ext]
   // then we will next instantly
-  if (context.url.pathname.match(ignorePattern) && import.meta.env.DEV) {
+  if (context.url.pathname.match(ignorePattern)) {
     return next();
   }
   // okay, now check if the it's the subdomain in `subdomains` array
@@ -22,7 +21,7 @@ export const onRequest: MiddlewareHandler = (context, next) => {
     const pathname = pathnames.join("/").replace(/^\/+|\/$/, "");
     const url = `${site.url.protocol}://${rest.join(".")}/app/${subdomain}/${pathname}`;
     console.log(url);
-    return rewrite(new URL(url), context.request);
+    return next(url);
   }
 
   return next();
