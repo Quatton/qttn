665f030b5397cf8ebb612c85ffe93ab45094a6a6
Quatton <atomicatives@gmail.com>
2024-09-25 23:23:24 +0900

    loading state

--- src/components/const/NewGameButton.vue
+++ src/components/const/NewGameButton.vue
@@ -1,26 +1,38 @@
 <script setup lang="ts">
 import { usePastGames } from "@/hooks/vue/useConstCode";
+import { useAsyncState } from "@vueuse/core";
 import { actions } from "astro:actions";
 import { navigate } from "astro:transitions/client";
 
 const { games } = usePastGames();
-async function startNewGame() {
-  await Promise.all(
-    games.value.map(async (game) => {
-      const content = localStorage.getItem(`const:code:${game}`);
-      if (content && content.trim().length > 0) {
-        await actions.constAction.updateGame({ id: game, content });
-      }
-      localStorage.removeItem(`const:code:${game}`);
-    }),
-  );
-  const { data } = await actions.constAction.new({});
-  if (data) navigate(`/game/${data}`);
-}
+const { execute, isLoading } = useAsyncState(
+  async () => {
+    await Promise.all(
+      games.value.map(async (game) => {
+        const content = localStorage.getItem(`const:code:${game}`);
+        if (content && content.trim().length > 0) {
+          await actions.constAction.updateGame({ id: game, content });
+        }
+        localStorage.removeItem(`const:code:${game}`);
+      }),
+    );
+    const { data } = await actions.constAction.new({});
+    if (data) navigate(`/game/${data}`);
+  },
+  undefined,
+  {
+    immediate: false,
+  },
+);
 </script>
 
 <template>
-  <button type="submit" class="btn btn-primary" @click="startNewGame">
+  <button
+    type="submit"
+    class="btn btn-primary"
+    @click="() => execute()"
+    :disabled="isLoading"
+  >
     Start a new game
   </button>
 </template>
