ce2a5de3c39428ed2503aa5d1d10bfb99130a7da
Quatton <atomicatives@gmail.com>
2024-09-25 14:10:59 +0900

    merge default

--- src/hooks/vue/useConstCode.ts
+++ src/hooks/vue/useConstCode.ts
@@ -3,7 +3,13 @@ import { computed } from "vue";
 
 export function useConstCode(id: string, defaultValue = "") {
   const key = `const:code:${id}`;
-  const code = useLocalStorage(key, defaultValue);
+  const code = useLocalStorage(key, defaultValue, {
+    mergeDefaults(storageValue, defaults) {
+      if (storageValue === undefined || storageValue.trim().length === 0)
+        return defaults;
+      return storageValue;
+    },
+  });
   return computed({
     get: () => code.value,
     set: (value: string) => {
