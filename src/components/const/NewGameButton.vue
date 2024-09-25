d9d6b02e2d1cafe68aee3755d44abea37cc25929
Quatton <atomicatives@gmail.com>
2024-09-25 15:20:00 +0900

    promise all

--- src/components/const/NewGameButton.vue
+++ src/components/const/NewGameButton.vue
@@ -1,21 +1,20 @@
 <script setup lang="ts">
-import { useConstCode, usePastGames } from "@/hooks/vue/useConstCode";
+import { usePastGames } from "@/hooks/vue/useConstCode";
 import { actions } from "astro:actions";
 import { navigate } from "astro:transitions/client";
 
-const { latestGame } = usePastGames();
+const { games } = usePastGames();
 async function startNewGame() {
-  if (latestGame.value) {
-    const content = localStorage.getItem(`const:code:${latestGame.value}`);
-    if (content) {
-      await actions.constAction
-        .updateGame({ id: latestGame.value, content })
-        .catch(console.error);
-      localStorage.removeItem(`const:code:${latestGame}`);
-    }
-  }
+  await Promise.all(
+    games.value.map(async (game) => {
+      const content = localStorage.getItem(`const:code:${game}`);
+      if (content && content.trim().length > 0) {
+        await actions.constAction.updateGame({ id: game, content });
+      }
+      localStorage.removeItem(`const:code:${game}`);
+    }),
+  );
   const { data } = await actions.constAction.new({});
-
   if (data) navigate(`/game/${data}`);
 }
 </script>
