aab0462cc82c7231042bb78774faf1f14a873fa8
Quatton <atomicatives@gmail.com>
2025-04-21 21:58:37 +0900

    make react19 happy

--- astro.config.mjs
+++ astro.config.mjs
@@ -57,12 +57,22 @@ export default defineConfig({
   ],
   output: "server",
   adapter: cloudflare({
-    imageService: "cloudflare",
+    imageService: "compile",
   }),
   vite: {
     ssr: {
       noExternal: ["monaco-editor"],
     },
     plugins: [tailwindcss()],
+    resolve: {
+      // https://github.com/withastro/adapters/pull/436#issuecomment-2525190557
+      // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
+      // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
+      alias: import.meta.env.PROD
+        ? {
+            "react-dom/server": "react-dom/server.edge",
+          }
+        : {},
+    },
   },
 });
