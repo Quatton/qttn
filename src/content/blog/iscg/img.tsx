e7b9781da713fc0ec67690fc3ef02bb794b81c71
Quatton <atomicatives@gmail.com>
2025-07-06 18:02:33 +0900

    rock

--- src/content/blog/iscg/img.tsx
+++ src/content/blog/iscg/img.tsx
@@ -1,3 +1,86 @@
+import { useRef, useState, type ComponentPropsWithRef } from "react";
+import rock from "./rock.png";
+
 export function Filter() {
-  return "hi";
+  const originalImgRef = useRef<HTMLImageElement>(null);
+  const smoothedImgRef = useRef<HTMLImageElement>(null);
+  const detailImgRef = useRef<HTMLImageElement>(null);
+  const enhancedImgRef = useRef<HTMLImageElement>(null);
+
+  const [{ width, height }, setDimensions] = useState(() => ({
+    width: rock.width,
+    height: rock.height,
+  }));
+
+  const aspectRatio = width / height;
+
+  return (
+    <div className="flex items-center justify-center">
+      <div className="not-prose grid aspect-square max-h-full max-w-full grid-cols-2 grid-rows-2 place-content-center gap-2">
+        <figure className="flex flex-col items-center justify-center gap-2">
+          <div style={{ aspectRatio }} className="w-full">
+            <ImageOrSkeleton
+              isProcessing={false}
+              src={rock.src}
+              ref={originalImgRef}
+              alt="Original rock texture image"
+              className="h-full w-full rounded object-contain"
+            />
+          </div>
+          <figcaption className="text-center text-sm font-medium">
+            Original
+          </figcaption>
+        </figure>
+        <figure className="flex flex-col items-center justify-center gap-2">
+          <div style={{ aspectRatio }} className="w-full">
+            <ImageOrSkeleton
+              ref={smoothedImgRef}
+              alt="Smoothed rock texture image"
+              className="h-full w-full rounded object-contain"
+            />
+          </div>
+          <figcaption className="text-center text-sm font-medium">
+            Smoothed
+          </figcaption>
+        </figure>
+        <figure className="flex flex-col items-center justify-center gap-2">
+          <div style={{ aspectRatio }} className="w-full">
+            <ImageOrSkeleton
+              ref={detailImgRef}
+              alt="Detail enhanced rock texture image"
+              className="h-full w-full rounded object-contain"
+            />
+          </div>
+          <figcaption className="text-center text-sm font-medium">
+            Detail
+          </figcaption>
+        </figure>
+        <figure className="flex flex-col items-center justify-center gap-2">
+          <div style={{ aspectRatio }} className="w-full">
+            <ImageOrSkeleton
+              ref={enhancedImgRef}
+              alt="Enhanced rock texture image"
+              className="h-full w-full rounded object-contain"
+            />
+          </div>
+          <figcaption className="text-center text-sm font-medium">
+            Enhanced
+          </figcaption>
+        </figure>
+      </div>
+    </div>
+  );
+}
+
+export function ImageOrSkeleton({
+  isProcessing = true,
+  ...imgProps
+}: ComponentPropsWithRef<"img"> & {
+  isProcessing?: boolean;
+}) {
+  return isProcessing ? (
+    <div className="h-full w-full animate-pulse rounded bg-gray-200" />
+  ) : (
+    <img {...imgProps} />
+  );
 }
