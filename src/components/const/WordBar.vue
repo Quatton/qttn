8a4ee270c64b63957edfd97d50f7dbd1c56c1c3e
Quatton <atomicatives@gmail.com>
2026-02-14 21:59:11 +0900

    format

--- src/components/const/WordBar.vue
+++ src/components/const/WordBar.vue
@@ -1,78 +1,88 @@
 <script setup lang="ts">
-import type { CompressedWord, CompressedWordWithMatch, GameSession } from "@/lib/const/rules";
-import { actions } from "astro:actions";
-import { onMounted, ref } from "vue";
-import { useAutoAnimate } from "@formkit/auto-animate/vue";
-import { useAsyncState } from "@vueuse/core";
-import WordBadge from "./WordBadge.vue";
-import { useInitWords } from "@/store/word";
-import { usePastGames } from "@/hooks/vue/useConstCode";
+  import { usePastGames } from "@/hooks/vue/useConstCode";
+  import type {
+    CompressedWord,
+    CompressedWordWithMatch,
+    GameSession,
+  } from "@/lib/const/rules";
+  import { useInitWords } from "@/store/word";
+  import { useAutoAnimate } from "@formkit/auto-animate/vue";
+  import { useAsyncState } from "@vueuse/core";
+  import { actions } from "astro:actions";
+  import { onMounted, ref } from "vue";
+  import WordBadge from "./WordBadge.vue";
 
-const $props = defineProps<{
-  words: CompressedWordWithMatch[];
-  game: GameSession;
-}>();
+  const $props = defineProps<{
+    words: CompressedWordWithMatch[];
+    game: GameSession;
+  }>();
 
-const wordStore = useInitWords($props.words);
+  const wordStore = useInitWords($props.words);
 
-const updateWord = (idx: number, word: CompressedWord) => {
-  wordStore.value[idx] = {
-    ...wordStore.value[idx],
-    ...word,
+  const updateWord = (idx: number, word: CompressedWord) => {
+    wordStore.value[idx] = {
+      ...wordStore.value[idx],
+      ...word,
+    };
   };
-};
 
-const isLoading = ref<number | false>(false);
+  const isLoading = ref<number | false>(false);
 
-const swapOutWord = async (idx: number, reason: "difficult" | "notAWord" | "inappropriate") => {
-  isLoading.value = idx;
-  try {
-    const { data } = await actions.constAction.swapOut({
-      gameId: $props.game.id,
-      wordId: wordStore.value[idx].id,
-      reason,
-    });
+  const swapOutWord = async (
+    idx: number,
+    reason: "difficult" | "notAWord" | "inappropriate",
+  ) => {
+    isLoading.value = idx;
+    try {
+      const { data } = await actions.constAction.swapOut({
+        gameId: $props.game.id,
+        wordId: wordStore.value[idx].id,
+        reason,
+      });
 
-    if (data) {
-      updateWord(idx, data);
+      if (data) {
+        updateWord(idx, data);
+      }
+    } catch (e) {
+      console.error(e);
+    } finally {
+      isLoading.value = false;
     }
-  } catch (e) {
-    console.error(e);
-  } finally {
-    isLoading.value = false;
-  }
-};
+  };
 
-const {
-  state: definitions,
-  execute,
-  isLoading: definitionLoading,
-} = useAsyncState(
-  async (word: string) => {
-    return await actions.constAction.dictionary.orThrow({ word });
-  },
-  null,
-  {
-    immediate: false,
-    resetOnExecute: true,
-  },
-);
+  const {
+    state: definitions,
+    execute,
+    isLoading: definitionLoading,
+  } = useAsyncState(
+    async (word: string) => {
+      return await actions.constAction.dictionary.orThrow({ word });
+    },
+    null,
+    {
+      immediate: false,
+      resetOnExecute: true,
+    },
+  );
 
-const defineWord = (word: string) => {
-  execute(0, word);
-};
+  const defineWord = (word: string) => {
+    execute(0, word);
+  };
 
-const [parent] = useAutoAnimate();
+  const [parent] = useAutoAnimate();
 
-const { addGame } = usePastGames();
+  const { addGame } = usePastGames();
 
-onMounted(() => {
-  addGame($props.game.id);
-});
+  onMounted(() => {
+    addGame($props.game.id);
+  });
 </script>
 
 <template>
-  <div class="flex flex-wrap items-end justify-center gap-2 @sm:gap-4" ref="parent">
+  <div
+    class="flex flex-wrap items-end justify-center gap-2 @sm:gap-4"
+    ref="parent"
+  >
     <WordBadge
       v-for="(word, idx) in wordStore"
       :key="word.id"
