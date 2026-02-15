77c07141b366322883202d7a7b1f1800c048cc19
Quatton <atomicatives@gmail.com>
2026-02-15 22:55:16 +0900

    eh not interesting

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,14 +1,14 @@
-import { defineCollection } from 'astro:content';
+import { defineCollection } from "astro:content";
 
+import { glob } from "astro/loaders";
+import { z } from "astro/zod";
 
-import { z } from 'astro/zod';
-import { glob } from 'astro/loaders';
-
-const blog = defineCollection({ 
+const blog = defineCollection({
   loader: glob({ pattern: "**/*.{mdx,md}", base: "./src/content/blog" }),
   schema: z.object({
     title: z.string(),
     createdAt: z.date(),
+    description: z.string().optional(),
     publishedAt: z.date().optional(),
   }),
 });
